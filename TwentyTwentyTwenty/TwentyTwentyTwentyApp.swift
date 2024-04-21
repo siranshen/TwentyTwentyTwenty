976b034e7cb68185519560db135941ba317c7534
Siran Shen <10250825+siranshen@users.noreply.github.com>
2024-04-21 12:18:21 +0800

    Initial Commit

---
+++ TwentyTwentyTwenty/TwentyTwentyTwentyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwentyTwentyTwentyApp.swift
+//  TwentyTwentyTwenty
+//
+//  Created by Siran on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TwentyTwentyTwentyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
